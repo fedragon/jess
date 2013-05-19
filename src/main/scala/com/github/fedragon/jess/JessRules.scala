57aa6b9c23713c3ba7468be1c27a3bca0384e4f7
Federico Ragona <maffo7@gmail.com>
2013-05-19 21:57:06 +0200

    Improved JsArrayRule (added support for different types of JsValue in the same array)

--- src/main/scala/com/github/fedragon/jess/JessRules.scala
+++ src/main/scala/com/github/fedragon/jess/JessRules.scala
@@ -50,21 +50,35 @@ case class JsArrayRule(rules: Seq[JsValueRule]) extends JsValueRule {
 
 	def apply(js: JsValue): Boolean = {
 
-		js match {
-			case array: JsArray =>
-				rules exists { rule =>
-					
-					array.value exists { next =>
-					 
-						next match {
-							case notFound: JsUndefined => 
-								false
-							case value @ _ =>
-								//println(s"Found: $value")
-								rule(value)
-						}
+		val results =
+			js match {
+				case array: JsArray =>
+					rules map { rule =>
+						
+						val fields = filter(rule, array.value)
+
+							fields exists { field =>
+							 
+								field match {
+									case notFound: JsUndefined => 
+										false
+									case value @ _ =>
+										rule(value)
+								}
+							}
 					}
-				}
+				case other => throw new IllegalArgumentException(s"Invalid input: $other")
+			}
+
+		results forall ( r => r == true )
+	}
+
+	private def filter(rule: JsValueRule, fields: Seq[JsValue]): Seq[JsValue] = {
+		rule match {
+			case a: JsArrayRule => fields.filter(f => f.isInstanceOf[JsArray])
+			case n: JsNumberRule => fields.filter(f => f.isInstanceOf[JsNumber])
+			case o: JsObjectRule => fields.filter(f => f.isInstanceOf[JsObject])
+			case s: JsStringRule => fields.filter(f => f.isInstanceOf[JsString])
 			case other => throw new IllegalArgumentException(s"Invalid input: $other")
 		}
 	}
